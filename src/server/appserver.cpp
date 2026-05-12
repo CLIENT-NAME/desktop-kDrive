e8e04a7dda9aca7fd8033849b9e9ea9f29df8366
Herve <herve.eruam@infomaniak.com>
2026-05-12 11:43:39 +0200

    fix(appserver): always return ExitInfo in updateUserInfo
    Ensure updateUserInfo returns ExitCode::Ok when drives is empty,
    improving code clarity and consistency by always returning a valid
    ExitInfo.

--- src/server/appserver.cpp
+++ src/server/appserver.cpp
@@ -2831,7 +2831,7 @@ ExitInfo AppServer::updateUserInfo(User &user) {
 
         if (drives.empty()) {
             deleteAccount(account.dbId());
-            return;
+            return ExitCode::Ok;
         }
 
         if (const auto exitInfo = updateAccount(account); !exitInfo) return exitInfo;
